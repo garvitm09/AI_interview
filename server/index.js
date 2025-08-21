4e85d86192134c4d5f482d6b56ebdc6c7fefb5de
garvitm09 <garvitm09@gmail.com>
2025-08-21 17:34:54 +0530

    CORS fix 2

--- server/index.js
+++ server/index.js
@@ -3,7 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+const serviceAccount = {
+  type: "service_account",
+  project_id:  "companion-642f2",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: "firebase-adminsdk-fbsvc@companion-642f2.iam.gserviceaccount.com",
+  client_id: "107358089868210607423",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40companion-642f2.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
+
 const app = express();
 const port = process.env.PORT || 5000;
 const interviewRoutes = require("./routes/interview");
